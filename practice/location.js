22a5e0ce7e0196f6402a9ef21e823c5463715e9d
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-09-05 10:23:08 +0600

    location file added

---
+++ practice/location.js
@@ -0,0 +1,9 @@
+const onReload = () => {
+    location.reload();
+}
+const onAssign = () => {
+    location.assign('https://www.google.com/');
+}
+const onHref = () => {
+    location.href = 'https://www.facebook.com/';
+}
